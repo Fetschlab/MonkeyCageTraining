718b9ff5622402e9e06d6e3a105650810d09f241
hkuperstein <harrykuperstein@gmail.com>
2018-09-05 14:52:25 -0400

    swipe recognition

--- CageTraining/src/tasks/touchdot/Stimulus.js
+++ CageTraining/src/tasks/touchdot/Stimulus.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import ZingTouch from 'zingtouch';
 import Circle from '../../generic/Circle';
 import Utils from '../../generic/Utils';
- class Stimulus extends Component {
-   constructor(props) {
+
+class Stimulus extends Component {
+
+  constructor(props) {
     super(props);
     this.state = {
       width: props.width,
@@ -10,27 +13,43 @@ import Utils from '../../generic/Utils';
       r: props.r,
       color: 'red',
       isFinalized: false,
+      zt: new ZingTouch.Region(document.getElementById('root')), // HK - sets up zingtouch region
     };
     this.finalize = this.finalize.bind(this);
   }
-   finalize() {
+
+  finalize() { // must try https://github.com/zingchart/zingtouch#regionbindelement
+    console.log(this.state.zt);
+    // let touchCheck = new ZingTouch.Tap({ tolerance: 125 });
     if(!this.state.isFinalized) {
-      let timestamp = Utils.getTimeStamp();
-      this.setState(prevState => ({
-        color: 'FireBrick',
-        isFinalized: true,
-      }));
-      let data = {timestamp: timestamp};
-      this.props.onFinalized(data);
+        let swipeCheck = false;
+        console.log(swipeCheck + ' before swipe');
+        this.state.zt.bind(document.getElementById('root'), 'swipe', function(e) {
+          let swipeCheck = true;
+          console.log(swipeCheck + ' at swipe');
+        }, false);
+        console.log(swipeCheck + ' after swipe');
+        let timestamp = Utils.getTimeStamp();
+        this.setState(prevState => ({
+          color: 'FireBrick',
+          isFinalized: true,
+        }));
+        let data = { timestamp: timestamp };
+        if(swipeCheck === false) {
+          this.props.onFinalized(data);
+        }
     }
   }
-   componentWillUnmount() {
+
+  componentWillUnmount() {
     this.interval && clearInterval(this.interval);
   }
-   componentDidMount() {
+
+  componentDidMount() {
     this.tsStartTrial = Utils.getTimeStamp();
   }
-   // if component is re-generated, this is called rather than the constructor:
+
+  // if component is re-generated, this is called rather than the constructor:
   componentWillReceiveProps(props) {
     this.setState(prevState => ({
       r: Math.max(prevState.r * 0.995, this.props.minr),
@@ -38,7 +57,8 @@ import Utils from '../../generic/Utils';
       isFinalized: false,
     }));
   }
-   render() {
+
+  render() {
     let self = this
     return (
       <svg width={this.props.width} height={this.props.height}>
@@ -55,4 +75,5 @@ import Utils from '../../generic/Utils';
     );
   }
 }
- export default Stimulus;
+
+export default Stimulus;
